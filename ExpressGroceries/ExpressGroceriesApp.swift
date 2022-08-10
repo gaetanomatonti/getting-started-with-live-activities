572160f6f2ff3aa912cde4bdb404521ce86ded98
gaetanomatonti <gaetanomatonti@icloud.com>
2022-08-10 14:01:27 +0200

    Initial Commit

---
+++ ExpressGroceries/ExpressGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressGroceriesApp.swift
+//  ExpressGroceries
+//
+//  Created by Gaetano Matonti on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpressGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
